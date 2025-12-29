0b854087c64082e8e4ca1a7b9db4e6970cf60e39
Peter Li <petercyli02@gmail.com>
2025-12-29 13:51:27 +0000

    feat: new favicon

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import faviconUrl from './assets/favicon.png'
+
+function setFavicon(href: string) {
+  let link = document.querySelector("link[rel~='icon']") as HTMLLinkElement | null
+  if (!link) {
+    link = document.createElement('link')
+    link.rel = 'icon'
+    document.head.appendChild(link)
+  }
+  link.type = 'image/png'
+  link.href = href
+}
+
+setFavicon(faviconUrl)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
